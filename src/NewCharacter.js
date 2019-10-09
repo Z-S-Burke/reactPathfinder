3078e268755275bfe4b5e81c8b8d2ac5f6d94c60
Z-S-Burke <zach@zsburke.com>
2019-10-09 15:55:02 +0200

    dramatically improved the aesthetic and clarity of the new character form

--- src/NewCharacter.js
+++ src/NewCharacter.js
@@ -8,15 +8,15 @@ class NewCharacter extends Component {
             <div>
                 <h1 className="bg-dark text-light my-2 pb-2 d-flex justify-content-center"> New Character </h1>
                 <form className="d-flex flex-column border border-dark py-3 px-3">
-                    <label className="w-100"> Name: <input type="text" placeholder="NAME" /> </label>
-                    <label> Class: <input type="text" placeholder="CLASS" /> </label>
-                    <label> Level: <input type="text" placeholder="LEVEL" /> </label>
-                    <label> Strength: <input type="text" placeholder="STRENGTH" /> </label>
-                    <label> Dexterity: <input type="text" placeholder="DEXTERITY" /> </label>
-                    <label> Constitution: <input type="text" placeholder="CONSTITUTION" /> </label>
-                    <label> Intelligence: <input type="text" placeholder="INTELLIGENCE" /> </label>
-                    <label> Wisdom: <input type="text" placeholder="WISDOM" /> </label>
-                    <label> Charisma: <input type="text" placeholder="CHARISMA" /> </label>
+                    <label> Name: <input className="w-100 border" type="text" placeholder="NAME" /> </label>
+                    <label> Class: <input className="w-100 border" type="text" placeholder="CLASS" /> </label>
+                    <label> Level: <input className="w-100 border" type="text" placeholder="LEVEL" /> </label>
+                    <label> Strength: <input className="w-100 border" type="text" placeholder="STRENGTH" /> </label>
+                    <label> Dexterity: <input className="w-100 border" type="text" placeholder="DEXTERITY" /> </label>
+                    <label> Constitution: <input className="w-100 border" type="text" placeholder="CONSTITUTION" /> </label>
+                    <label> Intelligence: <input className="w-100 border" type="text" placeholder="INTELLIGENCE" /> </label>
+                    <label> Wisdom: <input className="w-100 border" type="text" placeholder="WISDOM" /> </label>
+                    <label> Charisma: <input className="w-100 border" type="text" placeholder="CHARISMA" /> </label>
                     <input className="text-light bg-dark d-flex justify-content-center" type="submit" value="Submit" />
                 </form>
             </div>
