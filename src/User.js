a78230867a8fa122723200c5b3d06d9a4e7d574e
Z-S-Burke <zach@zsburke.com>
2019-10-15 15:56:27 +0200

    made a cleaner version of the simple login form

--- src/User.js
+++ src/User.js
@@ -8,13 +8,16 @@ class User extends Component {
     render() {
         return (
             <div>
-                <form className="d-flex flex-column border border-dark py-3 px-3">
-                    <div className="loginForm">
-                        <label> E-mail: <input className="border" type="text" /> </label>
-                        <label> Password: <input className="border" type="text" /> </label>
-                    </div>
-                    <input className="text-light bg-dark d-flex justify-content-center p-1" type="submit" value="Submit" />
-                </form>
+                <div className="d-flex flex-column align-items-center">
+                    <h2> Get started with Shopkeep </h2>
+                    <form className="d-flex flex-column py-3 px-3">
+                        <div className="loginForm">
+                            <label> E-mail: <input className="border w-100" type="text" /> </label>
+                            <label> Password: <input className="border w-100" type="text" /> </label>
+                        </div>
+                        <input className="text-light bg-dark d-flex justify-content-center p-1" type="submit" value="Submit" />
+                    </form>
+                </div>
             </div>
         );
     }
