614df5af18f4372136df57b270789a635c3062d8
Z-S-Burke <zach@zsburke.com>
2019-10-17 11:28:05 +0200

    added a register component for new user login

---
+++ src/Register.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import * as bs from 'bootstrap/dist/css/bootstrap.css';
+
+
+class Register extends Component {
+
+    render() {
+        return (
+            <div>
+                <div className="d-flex flex-column align-items-center">
+                    <form className="d-flex flex-column py-3 px-3">
+                        <div className="loginForm">
+                            <label> Username: <input className="border w-100" type="text" /> </label>
+                            <label> E-mail: <input className="border w-100" type="text" /> </label>
+                            <label> Password: <input className="border w-100" type="text" /> </label>
+                        </div>
+                        <input className="text-light bg-dark d-flex justify-content-center p-1" type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Register;
--- src/User.js
+++ src/User.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import * as bs from 'bootstrap/dist/css/bootstrap.css';
 
 
@@ -8,19 +8,22 @@ class User extends Component {
     render() {
         return (
             <div>
-                <div className="d-flex flex-column align-items-center">
-                    <form className="d-flex flex-column py-3 px-3">
-                        <div className="loginForm">
-                            <label> E-mail: <input className="border w-100" type="text" /> </label>
-                            <label> Password: <input className="border w-100" type="text" /> </label>
-                        </div>
-                        <input className="text-light bg-dark d-flex justify-content-center p-1" type="submit" value="Submit" />
-                        <div className="mt-3">
-                            <p> Don't have an account? </p>
-                            <Link> Click here to register. </Link>
-                        </div>
-                    </form>
-                </div>
+                <Router>
+                    <div className="d-flex flex-column align-items-center">
+                        <form className="d-flex flex-column py-3 px-3">
+                            <div className="loginForm">
+                                <label> E-mail: <input className="border w-100" type="text" /> </label>
+                                <label> Password: <input className="border w-100" type="text" /> </label>
+                            </div>
+                            <input className="text-light bg-dark d-flex justify-content-center p-1" type="submit" value="Submit" />
+                            <div className="mt-3">
+                                <p> Don't have an account? </p>
+                                <Link to="/register"> Click here to register. </Link>
+                            </div>
+                        </form>
+                    </div>
+                    <Route path="/router" component={Router} />
+                </Router>
             </div>
         );
     }
